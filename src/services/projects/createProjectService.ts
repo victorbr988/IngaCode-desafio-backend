8c1a993f46e7ae558b09bac309e8cf65737dd34e
Victor Gabriel <gabrielvghs666@gmail.com>
2022-10-15 14:37:20 -0300

    feat: add method to create project in service

---
+++ src/services/projects/createProjectService.ts
@@ -0,0 +1,22 @@
+import { Projects } from "@prisma/client";
+import { createProjectModel } from "../../models/projects/createProjectModel";
+
+export async function createProjectService(props: Projects): Promise<string> {
+  const {
+    id,
+    name,
+    createdAt,
+    updatedAt,
+    deletedAt
+  } = props
+
+  const idProject = await createProjectModel({
+    id,
+    name,
+    createdAt,
+    updatedAt,
+    deletedAt
+  })
+
+  return idProject
+}
