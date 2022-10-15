f200e1848703518942f9ccff1384e526caa2b45d
Victor Gabriel <gabrielvghs666@gmail.com>
2022-10-15 19:00:12 -0300

    refactor: alter import

--- src/controller/projects/findAllProjectsController.ts
+++ src/controller/projects/findAllProjectsController.ts
@@ -1,7 +1,7 @@
 import { Projects } from "@prisma/client";
 import { Request, Response } from "express";
 import { findAllProjectsService } from "../../services/projects/findAllProjectsService";
-import { statusCode } from "../../utils";
+import { statusCode } from "../../utils/StatusCode";
 
 export async function findAllProjectsController(_request: Request, response: Response): Promise<Response> {
   const allProjects: Projects[] = await findAllProjectsService()
