86c6697820ddbf5c66563681494469cdd14bdbec
Victor Gabriel <gabrielvghs666@gmail.com>
2022-10-15 18:58:11 -0300

    feat: add new routes and middlewares on routes

--- src/server.ts
+++ src/server.ts
@@ -3,13 +3,16 @@ const cors = require('cors');
 import 'dotenv/config'
 import { tasksRouter } from "./routers/tasks";
 import { projectsRoute } from "./routers/projects";
+import { userRouter } from "./routers/users";
+import { authUser, validateUser } from "./Middleware/authUser";
 const app = express()
 
 const port = process.env.PORT
 app.use(express.json());
 app.use(cors())
 
-app.use('/tasks', tasksRouter)
-app.use('/project', projectsRoute)
+app.use('/tasks', authUser, tasksRouter)
+app.use('/project', authUser, projectsRoute)
+app.use('/user', validateUser, userRouter)
 
 app.listen(port, () => console.log(`listening on port ${port}`))
