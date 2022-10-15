ddf86947d7751b37df0b57198ec0901c069c38d2
Victor Gabriel <gabrielvghs666@gmail.com>
2022-10-15 14:37:45 -0300

    feat: add method to find all projects in service

---
+++ src/services/projects/findAllProjectsService.ts
@@ -0,0 +1,8 @@
+import { Projects } from "@prisma/client";
+import { findAllProjectsModel } from "../../models/projects/findAllProjectsModel";
+
+export async function findAllProjectsService(): Promise<Projects[]> {
+  const allProjects: Projects[] = await findAllProjectsModel()
+
+  return allProjects;
+}
