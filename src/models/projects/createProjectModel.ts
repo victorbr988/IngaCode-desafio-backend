7181ab22e08c0b5d571d467b72bafa40a1cf3857
Victor Gabriel <gabrielvghs666@gmail.com>
2022-10-15 14:35:40 -0300

    feat: add method to create project in model

---
+++ src/models/projects/createProjectModel.ts
@@ -0,0 +1,24 @@
+import { PrismaClient, Projects } from "@prisma/client";
+import { v4 } from "uuid";
+
+const prisma = new PrismaClient()
+
+export async function createProjectModel(props: Projects): Promise<string> {
+  const {
+    name,
+    createdAt,
+    updatedAt,
+    deletedAt
+  } = props
+
+  const project = await prisma.projects.create({
+    data: {
+      id: v4(),
+      name,
+      createdAt,
+      updatedAt,
+      deletedAt
+    }
+  })
+  return project.id
+}
