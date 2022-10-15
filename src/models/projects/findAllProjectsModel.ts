c215ff5afedb2388ff391c9842cd91818058480d
Victor Gabriel <gabrielvghs666@gmail.com>
2022-10-15 14:36:25 -0300

    feat: add method to find all project in model

---
+++ src/models/projects/findAllProjectsModel.ts
@@ -0,0 +1,9 @@
+import { PrismaClient, Projects } from "@prisma/client";
+
+const prisma = new PrismaClient()
+
+export async function findAllProjectsModel(): Promise<Projects[]> {
+  const allProjects: Projects[] = await prisma.projects.findMany();
+
+  return allProjects
+}
