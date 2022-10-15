6b57beb6e39e0a0ed9c5c7a147ff3d003991bc1c
Victor Gabriel <gabrielvghs666@gmail.com>
2022-10-15 19:05:07 -0300

    feat: add function to encrypted password

---
+++ src/utils/bcrypt.ts
@@ -0,0 +1,7 @@
+import bcrypt from 'bcrypt'
+
+export async function encrypted (password: string): Promise<string> {
+  const passwordEncrypted = await bcrypt.hash(password, 10);
+
+  return passwordEncrypted;
+}
