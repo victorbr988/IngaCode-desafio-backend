d9dbc1dd9b340dd50fe845f762edc857a31864b3
Victor Gabriel <gabrielvghs666@gmail.com>
2022-10-15 14:34:49 -0300

    feat: add method to findAll project in controller

---
+++ src/controller/projects/findAllProjectsController.ts
@@ -0,0 +1,10 @@
+import { Projects } from "@prisma/client";
+import { Request, Response } from "express";
+import { findAllProjectsService } from "../../services/projects/findAllProjectsService";
+import { statusCode } from "../../utils";
+
+export async function findAllProjectsController(_request: Request, response: Response): Promise<Response> {
+  const allProjects: Projects[] = await findAllProjectsService()
+
+  return response.status(statusCode.OK).json(allProjects)
+}
