8bddf44713caf9af0648e4a20bb9f73306e70dfd
Victor Gabriel <gabrielvghs666@gmail.com>
2022-10-16 11:30:37 -0300

    style: add semicolon and blank lines

--- src/Middleware/authUser.ts
+++ src/Middleware/authUser.ts
@@ -4,38 +4,38 @@ import { validateToken } from "../utils/token";
 import { PrismaClient, Users } from "@prisma/client";
 
 export function authUser(request: Request, response: Response, next: NextFunction): Response | void {
-  const { authorization } = request.headers
+  const { authorization } = request.headers;
 
   if (!authorization) {
     return response.status(statusCode.NOT_FOUNT).json({ message: "Token not found" });
-  }
+  };
 
   try {
     validateToken(authorization);
   } catch(err) {
-    return response.status(statusCode.INVALID_DATA).json({ message: "invalid or expired token"})
-  }
+    return response.status(statusCode.INVALID_DATA).json({ message: "invalid or expired token"});
+  };
 
-  next()
-}
+  next();
+};
 
 export async function validateUser(request: Request, response: Response, next: NextFunction) {
   const { userName, password } = request.body;
-  const prisma = new PrismaClient()
+  const prisma = new PrismaClient();
 
   if (!userName || !password) {
-    return response.status(statusCode.INVALID_DATA).json({ message: "E-mail or password invalid"})
-  }
+    return response.status(statusCode.INVALID_DATA).json({ message: "E-mail or password invalid"});
+  };
 
   const user: Users | null = await prisma.users.findUnique({
     where: {
       userName
-    }
-  })
+    },
+  });
 
   if (user) {
-    return response.status(statusCode.INVALID_DATA).json({ message: "Username alread exists" })
-  }
+    return response.status(statusCode.INVALID_DATA).json({ message: "Username alread exists" });
+  };
 
-  next()
-}
+  next();
+};
