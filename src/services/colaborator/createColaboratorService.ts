68b09029ab5aea181bfe0b9ca299407ae6c7d0fd
Victor Gabriel <gabrielvghs666@gmail.com>
2022-10-16 11:33:43 -0300

    feat: add method to create colaborator in service

---
+++ src/services/colaborator/createColaboratorService.ts
@@ -0,0 +1,10 @@
+import { createColaboratorModel } from "../../models/colaborator/createColaboratorModel";
+import { ColaboratorProps } from "../../utils/types";
+
+export async function createColaboratorService(props: ColaboratorProps): Promise<string> {
+  const { name } = props;
+
+  const colaboratorId: string = await createColaboratorModel({ name });
+
+  return colaboratorId;
+}
