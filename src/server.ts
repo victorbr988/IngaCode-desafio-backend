efca881dcedb3997d42d5a9d73ddc9c13810062c
Victor Gabriel <gabrielvghs666@gmail.com>
2022-10-16 11:29:24 -0300

    feat: add route to colaborator

--- src/server.ts
+++ src/server.ts
@@ -5,14 +5,16 @@ import { tasksRouter } from "./routers/tasks";
 import { projectsRoute } from "./routers/projects";
 import { userRouter } from "./routers/users";
 import { authUser, validateUser } from "./Middleware/authUser";
-const app = express()
+import { colaboratorRoute } from "./routers/colaborator";
 
-const port = process.env.PORT
+const app = express();
+
 app.use(express.json());
-app.use(cors())
+app.use(cors());
+app.use('/tasks', authUser, tasksRouter);
+app.use('/project', authUser, projectsRoute);
+app.use('/user', validateUser, userRouter);
+app.use('/colaborator', authUser, colaboratorRoute);
 
-app.use('/tasks', authUser, tasksRouter)
-app.use('/project', authUser, projectsRoute)
-app.use('/user', validateUser, userRouter)
-
-app.listen(port, () => console.log(`listening on port ${port}`))
+const port = process.env.PORT;
+app.listen(port, () => console.log(`listening on port ${port}`));
