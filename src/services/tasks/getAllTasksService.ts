342841c92a32bfde3105a6cd1d01d281652bbb66
Victor Gabriel <gabrielvghs666@gmail.com>
2022-10-16 19:02:14 -0300

    feat: add method to get all tasks in service

---
+++ src/services/tasks/getAllTasksService.ts
@@ -0,0 +1,8 @@
+import { Tasks } from "@prisma/client";
+import { getAllTasksModel } from "../../models/tasks/getAllTasksModel";
+
+export async function getAllTasksService(): Promise<Tasks[]> {
+  const allTasks = await getAllTasksModel();
+
+  return allTasks
+};
