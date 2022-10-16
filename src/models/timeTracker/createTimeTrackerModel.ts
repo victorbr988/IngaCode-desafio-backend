01a06c78466c21fe4a9ed8de052cc0dfab22e50b
Victor Gabriel <gabrielvghs666@gmail.com>
2022-10-16 15:25:33 -0300

    feat: method to create timeTracker in model

---
+++ src/models/timeTracker/createTimeTrackerModel.ts
@@ -0,0 +1,30 @@
+import { PrismaClient, TimeTrackers } from "@prisma/client";
+import { v4 } from "uuid";
+import { TimeTrackerProps } from "../../utils/types";
+
+const prisma = new PrismaClient();
+
+export async function createTimeTrackerModel(props: TimeTrackerProps) {
+  const {
+    startDate,
+    endDate,
+    taskId,
+    colaboratorId
+  } = props;
+
+  const date: string[] = Date().split(' ');
+
+
+  const timeTracker: TimeTrackers = await prisma.timeTrackers.create({
+    data: {
+      id: v4(),
+      startDate: new Date(startDate),
+      endDate: new Date(endDate),
+      timeZoneId: date[5],
+      taskId,
+      colaboratorId
+    }
+  })
+
+  return timeTracker.id;
+}
