404a18b8d6ed38631b1c82b3f7d3708fb1dea9a0
Victor Gabriel <gabrielvghs666@gmail.com>
2022-10-16 19:02:51 -0300

    feat: include route get in config tasks

--- src/routers/tasks.ts
+++ src/routers/tasks.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import { createTaskController } from "../controller/tasks/createTask";
+import { getAllTasksController } from "../controller/tasks/getAllTaskscontroller";
 
 export const tasksRouter = express.Router();
 
 tasksRouter.post('/', createTaskController)
+tasksRouter.get('/', getAllTasksController)
