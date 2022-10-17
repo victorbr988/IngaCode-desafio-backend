a5a451540cb02b0f6ba8f80b6bc21315b3654c45
Victor Gabriel <gabrielvghs666@gmail.com>
2022-10-17 12:07:27 -0300

    feat: add  method to delete task

--- src/controller/tasks/deleteTaskController.ts
+++ src/controller/tasks/deleteTaskController.ts
@@ -2,7 +2,7 @@ import { Request, Response } from "express";
 import { deleteTaskService } from "../../services/tasks/deleteTaskService";
 import { statusCode } from "../../utils/StatusCode";
 
-export async function deleteTaskController(request: Request, response: Response) {
+export async function deleteTaskController(request: Request, response: Response): Promise<Response> {
   const { id } = request.params;
 
   await deleteTaskService(id);
