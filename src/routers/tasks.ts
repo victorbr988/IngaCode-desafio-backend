b161f795894c7cf6fa36e60ea3bf9b63dd150713
Victor Gabriel <gabrielvghs666@gmail.com>
2022-10-17 07:50:55 -0300

    feat: add config to create route update

--- src/routers/tasks.ts
+++ src/routers/tasks.ts
@@ -1,8 +1,10 @@
 import express from "express";
-import { createTaskController } from "../controller/tasks/createTask";
+import { createTaskController } from "../controller/tasks/createTaskController";
 import { getAllTasksController } from "../controller/tasks/getAllTaskscontroller";
+import { updateTaskController } from "../controller/tasks/updateTaskController";
 
 export const tasksRouter = express.Router();
 
 tasksRouter.post('/', createTaskController)
 tasksRouter.get('/', getAllTasksController)
+tasksRouter.put('/:id', updateTaskController)
