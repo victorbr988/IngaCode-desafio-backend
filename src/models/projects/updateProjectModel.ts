53eff7af38eba1f3be7ed29756124c410fccec68
Victor Gabriel <gabrielvghs666@gmail.com>
2022-10-17 12:19:11 -0300

    feat: add method to update project in model

---
+++ src/models/projects/updateProjectModel.ts
@@ -0,0 +1,28 @@
+import { PrismaClient, Projects } from "@prisma/client";
+import { v4 } from "uuid";
+
+const prisma = new PrismaClient();
+
+export async function updateProjectModel(props: Projects): Promise<string> {
+  const {
+    id,
+    name,
+    createdAt,
+    updatedAt,
+    deletedAt
+  } = props;
+
+  const project = await prisma.projects.update({
+    where: {
+      id
+    },
+    data: {
+      id: v4(),
+      name,
+      createdAt,
+      updatedAt,
+      deletedAt
+    },
+  });
+  return project.id;
+};
