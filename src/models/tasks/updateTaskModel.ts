e5414219fccf0ff925ca67cfb9f8d33aff467913
Victor Gabriel <gabrielvghs666@gmail.com>
2022-10-17 07:52:49 -0300

    feat: add method to update task in model

---
+++ src/models/tasks/updateTaskModel.ts
@@ -0,0 +1,33 @@
+import { PrismaClient, Tasks } from "@prisma/client";
+import { v4 } from 'uuid';
+
+const prisma = new PrismaClient()
+
+export async function updateTaskModel(props: Tasks): Promise<string> {
+  const {
+    id,
+    name,
+    description,
+    projectId,
+    colaboratorId,
+    createdAt,
+    updatedAt,
+    deletedAt
+  } = props;
+
+  const task = await prisma.tasks.update({
+    where: {
+      id
+    },
+    data: {
+      name,
+      description,
+      projectId,
+      colaboratorId,
+      createdAt,
+      updatedAt,
+      deletedAt
+    }
+  })
+  return task.id
+}
