b57d50590329467017175015e7e52c3736a3743e
Victor Gabriel <gabrielvghs666@gmail.com>
2022-10-17 13:29:12 -0300

    feat: add method to delete project in controller

---
+++ src/controller/projects/deleteProjectController.ts
@@ -0,0 +1,11 @@
+import { Request, Response } from "express";
+import { deleteProjectService } from "../../services/projects/deleteProjectService";
+import { statusCode } from "../../utils/StatusCode";
+
+export async function deleteProjectController(request: Request, response: Response): Promise<Response> {
+  const { id } = request.params;
+
+  await deleteProjectService(id)
+
+  return response.status(statusCode.OK).json({ message: "Project deleted!" });
+};
