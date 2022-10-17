5d60e13b741c3a6a545ca1310e9e8b1bd027a285
Victor Gabriel <gabrielvghs666@gmail.com>
2022-10-17 12:07:06 -0300

    feat: add route to delete task

--- src/routers/tasks.ts
+++ src/routers/tasks.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import { createTaskController } from "../controller/tasks/createTaskController";
+import { deleteTaskController } from "../controller/tasks/deleteTaskController";
 import { getAllTasksController } from "../controller/tasks/getAllTaskscontroller";
 import { updateTaskController } from "../controller/tasks/updateTaskController";
 
@@ -8,4 +9,4 @@ export const tasksRouter = express.Router();
 tasksRouter.post('/', createTaskController);
 tasksRouter.get('/', getAllTasksController);
 tasksRouter.put('/:id', updateTaskController);
-tasksRouter.delete('/:id', updateTaskController);
+tasksRouter.delete('/:id', deleteTaskController);
