4ad385f9d13e143907131081312a33f72584fc98
Victor Gabriel <gabrielvghs666@gmail.com>
2022-10-17 12:19:43 -0300

    feat: add route to update project

--- src/routers/projects.ts
+++ src/routers/projects.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import { createProjectController } from "../controller/projects/createProjectController";
 import { findAllProjectsController } from "../controller/projects/findAllProjectsController";
+import { updateProjectController } from "../controller/projects/updateProjectController";
 
 export const projectsRoute = express.Router();
 
 projectsRoute.post('/', createProjectController);
 projectsRoute.get('/', findAllProjectsController);
+projectsRoute.put('/:id', updateProjectController);
