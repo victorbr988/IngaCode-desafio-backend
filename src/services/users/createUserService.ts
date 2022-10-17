f426b279d4e8cd73824037abfd9349a69be0fd2a
Victor Gabriel <gabrielvghs666@gmail.com>
2022-10-17 07:46:24 -0300

    refactor: replace create to login

--- src/services/users/createUserService.ts
+++
@@ -1,6 +0,0 @@
-import { Users } from "@prisma/client";
-import { createUserModel } from "../../models/users/createUserModel";
-
-export async function createUserService(props: Users): Promise<void> {
-  await createUserModel(props);
-}
